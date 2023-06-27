7af935ac56b7df2aaf6530440849a6b4c5986458
jamesev15 <jamesev1502@gmail.com>
2023-06-27 16:42:12 -0500

    decoding content

--- batch/process.py
+++ batch/process.py
@@ -9,7 +9,7 @@ def process_s3_event() -> None:
     response = boto3.client("s3").get_object(
         Bucket=bucket_name, Key=object_key
     )
-    content = response["Body"].read()
+    content = response["Body"].read().decode("utf-8")
 
     print(f"{bucket_name} - {object_key} | {content}")
 
